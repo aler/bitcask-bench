212167effc6bb29e348c6349f3b10aa2ecd43ba6
James Mills <1290234+prologic@users.noreply.github.com>
2019-03-23 12:32:20 +1000

    Set default engine to bitcask

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	engine       = flag.String("e", "pogreb", "database engine name. pogreb, goleveldb, bbolt, badger, or bitcask")
+	engine       = flag.String("e", "bitcask", "database engine name. pogreb, goleveldb, bbolt, badger, or bitcask")
 	numKeys      = flag.Int("n", 100000, "number of keys")
 	minKeySize   = flag.Int("mink", 16, "minimum key size")
 	maxKeySize   = flag.Int("maxk", 64, "maximum key size")
